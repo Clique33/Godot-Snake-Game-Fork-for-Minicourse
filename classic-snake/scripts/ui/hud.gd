c2ebf8b72116fe6698993341a1160368a34ecfd7
Clique33 <gabriegcc@gmail.com>
2025-10-20 19:29:05 -0300

    Hud adicionada, bem como várias outras pequenas modificações de organização dos arquivos

---
+++ classic-snake/scripts/ui/hud.gd
@@ -0,0 +1,28 @@
+extends Control
+class_name Hud
+
+var initial_time : int
+
+@onready var time_manager: TimeManager = $TimeManager
+@onready var time_label: Label = $HSplitContainer/HBoxContainer2/TimeLabel
+@onready var score_label: Label = $HSplitContainer/HBoxContainer/ScoreLabel
+
+
+func _ready() -> void:
+	initial_time = Time.get_ticks_msec()
+
+
+func set_score(score : int) -> void:
+	score_label.text = "score : "+str(score)
+
+
+func update_time() -> void:
+	time_label.text = "time : "+str(get_current_timestamp())
+
+
+func get_current_timestamp() -> String:
+	return time_manager.get_time_stamp(Time.get_ticks_msec()-initial_time)
+
+
+func _on_display_time_timer_timeout() -> void:
+	time_label.text = "time : "+str(get_current_timestamp())
